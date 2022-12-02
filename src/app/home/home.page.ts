824de0e98fa99bfae05aee0a7110c320f1086df2
Caio Gottmann <caiogottmann@gmail.com>
2022-12-01 21:50:44 -0300

    Adicionando classificacao do IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,32 +7,44 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  weight: number = 0
-  height: number = 0
-  imc: number = 0
+  weight: number = 0;
+  height: number = 0;
 
   constructor(private toastCtrl: ToastController) {}
 
   onCalculate() {
     if (this.height <= 0 || this.weight <= 0) {
-      return
+      return;
     }
-    
-    this.imc = this.weight / (this.height * this.height)
-    this.showIMC()
+
+    const imc = this.weight / (this.height * this.height);
+    const classification = this.getClassificationIMC(imc);
+
+    this.showIMC({ imc, classification });
   }
 
-  async showIMC() {
+  getClassificationIMC(imc: number) {
+    let message = '';
+    if (imc < 18.5) message = 'Magreza';
+    else if (imc >= 18.5 && imc <= 24.9) message = 'Normal';
+    else if (imc >= 25 && imc <= 29.9) message = 'Sobrepeso';
+    else if (imc >= 30 && imc <= 39.9) message = 'Obesidade';
+    else if (imc >= 40) message = 'Obesidade Grave';
+    return message;
+  }
+
+  async showIMC({
+    imc,
+    classification,
+  }: {
+    imc: number;
+    classification: string;
+  }) {
     const toast = await this.toastCtrl.create({
-      message: `IMC = ${this.imc.toFixed(2)}`,
+      message: `${imc.toFixed(2)} - ${classification}`,
       duration: 3000,
-      color: 'secondary'
-    })
-    
-    toast.present()
-
+      color: 'secondary',
+    });
+    toast.present();
   }
-
-
 }
